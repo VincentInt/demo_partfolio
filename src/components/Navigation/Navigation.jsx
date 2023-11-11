ec748e93b2aac8ad6778e9be6915176525cdc70b
Alexander <sanyabobiev@gmail.com>
2023-11-11 20:33:34 +0300

    edit

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,6 +2,7 @@ import Btn from '../UI/Btn/Btn';
 import style from './Navigation.module.scss';
 import iconUppArrow from '../../assets/icons/move-to-the-next-page-symbol-svgrepo-com.svg';
 import imgAvatar from '../../assets/img/image_2023-11-08_22-40-47.png';
+
 const Navigation = () => {
   return (
     <div className={style.containerNav}>
